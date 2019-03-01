35ef3bd7c8792e04dd273b6a1d9adc1e98916c13
Жанара <@DESKTOP-O9KC10I>
2019-03-01 17:17:57 +0600

    Add project files.

---
+++ HW_28.02.19/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_28._02._19
+{
+    public class CountWords
+    {
+        public static Dictionary<string, int> dict;
+
+        public CountWords()
+        {
+            dict = new Dictionary<string, int>();
+        }
+
+        public void AddWord(string word)
+        {
+            if (dict.ContainsKey(word))
+            {
+                dict[word] += 1;
+            }
+            else
+            {
+                dict.Add(word, 1);
+            }
+        }
+
+        public void PrintStats()
+        {
+            foreach (var i in dict)
+            {
+                Console.WriteLine(" {0} - {1} раз.", i.Key, i.Value);
+            }
+        }
+    }
+}
